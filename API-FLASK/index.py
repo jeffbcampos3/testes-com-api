23d6e932dafdd74c5e4c2d9fabed428a700b1f79
Jeferson Vieira Borges de Campos <jeffbcampos@gmail.com>
2023-03-15 11:57:59 -0300

    Add files via upload

--- API-FLASK/index.py
+++ API-FLASK/index.py
@@ -7,13 +7,17 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from psycopg2 import Error
 from bcrypt import hashpw, gensalt, checkpw
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from datetime import timedelta
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 
 try:
     con = Conexao(host=os.getenv("HOST"), user=os.getenv("USER"), password=os.getenv("PASSWORD"), port=os.getenv("PORT"), database=os.getenv("DATABASE"))   
         
     app = Flask(__name__)
     app.config['JWT_SECRET_KEY'] = 'fcfded42-c0fc-11ed-afa1-0242ac120002'
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=15)
+    app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)
+    
     jwt = JWTManager(app)
     
     CORS(app)
@@ -23,15 +27,17 @@ try:
     def home():
         return "API ainda não explodiu"
     
-    @app.route("/filmes/<int:id>", methods =['GET' ,'POST'])
-    def consultarFilmes(id):      
+    @app.route("/filmes", methods =['GET' ,'POST'])
+    @jwt_required()
+    def consultarFilmes():      
       if(request.method == 'GET'):
+        id = get_jwt_identity()
         sql = f"SELECT * FROM filmes WHERE id_usuario = '{id}'"
-        results = con.querySelect(sql)        
+        results = con.querySelect(sql)               
         return results
       elif(request.method == 'POST'):
         titulo = request.json['titulo']
-        id_usuario = request.json['id_usuario']
+        id_usuario = get_jwt_identity()
         sql = f"SELECT * FROM filmes WHERE titulo = '{titulo}' AND id_usuario = '{id_usuario}'"
         resposta = con.querySelect(sql)[0]        
         if(resposta is None):
@@ -39,15 +45,17 @@ try:
         else:
           return jsonify({'status' : 'sucess'})
           
-    @app.route("/series/<int:id>", methods =['GET', 'POST'])
-    def consultarSeries(id):      
+    @app.route("/series", methods =['GET', 'POST'])
+    @jwt_required()
+    def consultarSeries():      
       if(request.method == 'GET'):
+        id = get_jwt_identity()
         sql = f"SELECT * FROM series WHERE id_usuario = '{id}'"        
         results = con.querySelect(sql)
         return results
       elif(request.method == 'POST'):
         titulo = request.json['titulo']
-        id_usuario = request.json['id_usuario']
+        id_usuario = get_jwt_identity()
         sql = f"SELECT * FROM series WHERE titulo = '{titulo}' AND id_usuario = '{id_usuario}'"        
         resposta = con.querySelect(sql)
         if(resposta == []):
@@ -55,8 +63,10 @@ try:
         else:
           return jsonify({'status' : 'sucess'})
     
-    @app.route("/listaDesejo/<int:id>", methods =['GET'])
-    def consultarListaDesejo(id):
+    @app.route("/listaDesejo", methods =['GET'])
+    @jwt_required()
+    def consultarListaDesejo():
+      id = get_jwt_identity()
       sql = f"SELECT * FROM listaDesejo WHERE id_usuario = '{id}'"
       results = con.querySelect(sql)      
       return results
@@ -71,18 +81,19 @@ try:
         return jsonify({'status' : 'fail'})
       else:
         resposta = resposta[0]
-        if checkpw(senha, resposta[3].encode('utf-8')):
-          access_token = create_access_token(identity=email)       
+        if checkpw(senha, resposta[3].encode('utf-8')):          
+          access_token = create_access_token(identity=resposta[0])       
           return jsonify({'status' : 'sucess', 'id': f'{resposta[0]}', 'nome' : f'{resposta[1]}', 'access_token': f'{access_token}'})        
         else:
           return jsonify({'status' : 'fail'})
     
     @app.route('/atualizarUsuario', methods=['POST'])
+    @jwt_required()
     def atualizar_user():        
         nome = request.json['nome']
         email = request.json['email']
         senha = request.json['senha']
-        id_usuario = request.json['id_usuario']
+        id_usuario = get_jwt_identity()
         if verificaSenha(senha):
           senha = senha.encode('utf-8')
           salt = gensalt()
@@ -96,45 +107,48 @@ try:
                         
     
     @app.route("/inserirFilme", methods =['POST'])
+    @jwt_required()
     def inserirFilme():
       titulo = request.json['titulo']
       imagem = request.json['imagem']
       nota = request.json['nota']
       tipo = request.json['tipo']
       id_api = request.json['id_api']
-      id_usuario = request.json['id_usuario']
+      id_usuario = get_jwt_identity()
       sql = f"INSERT INTO filmes (titulo, imagem, nota, tipo, id_api, id_usuario) VALUES (%s, %s, %s, %s, %s, %s)"
       values = titulo, imagem, nota, tipo, id_api, id_usuario
       con.queryExecute(sql, values)           
       return jsonify({'status': 'sucess'})
     
     @app.route("/inserirSerie", methods =['POST'])
+    @jwt_required()
     def inserirSerie():
       titulo = request.json['titulo']
       imagem = request.json['imagem']
       nota = request.json['nota']
       tipo = request.json['tipo']
       id_api = request.json['id_api']
-      id_usuario = request.json['id_usuario']      
+      id_usuario = get_jwt_identity()      
       sql = f"INSERT INTO series (titulo, imagem, nota, tipo, id_api, id_usuario) VALUES (%s, %s, %s, %s, %s, %s)"
       values = titulo, imagem, nota, tipo, id_api, id_usuario
       con.queryExecute(sql, values)           
       return jsonify({'status': 'sucess'})
     
     @app.route("/inserirListaDesejo", methods =['POST'])
+    @jwt_required()
     def inserirListaDesejo():
       titulo = request.json['titulo']
       imagem = request.json['imagem']
       nota = request.json['nota']
       tipo = request.json['tipo']
       id_api = request.json['id_api']
-      id_usuario = request.json['id_usuario']
+      id_usuario = get_jwt_identity()
       sql = f"INSERT INTO listadesejo (titulo, imagem, nota, tipo, id_api, id_usuario) VALUES (%s, %s, %s, %s, %s, %s)"
       values = (titulo, imagem, nota, tipo, id_api, id_usuario)
       con.queryExecute(sql, values)       
       return jsonify({'status': 'sucess'})
     
-    @app.route("/inserirUsuario", methods =['POST'])
+    @app.route("/inserirUsuario", methods =['POST'])    
     def inserirUsuario():
       nome = request.json['nome']
       email = request.json['email']
@@ -156,9 +170,10 @@ try:
         return jsonify({'status': 'fail'})
     
     @app.route("/removerListaDesejo", methods =['POST'])
+    @jwt_required()
     def removerListaDesejo():
       titulo = request.json['titulo']
-      id_usuario = request.json['id_usuario']
+      id_usuario = get_jwt_identity()
       sql = f"SELECT * FROM listaDesejo WHERE titulo = '{titulo}' AND id_usuario = '{id_usuario}' "
       resposta = con.querySelect(sql)          
       if(resposta is []):
@@ -169,24 +184,27 @@ try:
         return jsonify({'status': 'sucess'})
     
     @app.route("/removerFilme", methods = ['POST'])
+    @jwt_required()
     def removerFilme():
       titulo = request.json['titulo']
-      id_usuario = request.json['id_usuario']
+      id_usuario = get_jwt_identity()
       sql = f"DELETE FROM filmes WHERE id_usuario = '{id_usuario}' AND titulo = '{titulo}'"
       con.queryExecute(sql, values=None)          
       return jsonify({'status' : 'sucess'})
     
     @app.route("/removerSerie", methods = ['POST'])
+    @jwt_required()
     def removerSerie():
       titulo = request.json['titulo']
-      id_usuario = request.json['id_usuario']
+      id_usuario = get_jwt_identity()
       sql = f"DELETE FROM series WHERE id_usuario = '{id_usuario}' AND titulo = '{titulo}'"
       con.queryExecute(sql, values=None)      
       return jsonify({'status' : 'sucess'})
     
     @app.route("/deletarUsuario", methods = ['POST'])
+    @jwt_required()
     def deletarUsuario():
-      id_usuario = request.json['id_usuario']
+      id_usuario = get_jwt_identity()
       sql = f'''DELETE FROM filmes WHERE id_usuario = '{id_usuario}';
       DELETE FROM series WHERE id_usuario = '{id_usuario}';
       DELETE FROM listaDesejo WHERE id_usuario = '{id_usuario}';
